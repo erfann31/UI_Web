87eedc282ed74d41dfa70db4aea4fa3077d43bdb
erfann31 <erfannasri2@gmail.com>
2024-06-01 07:24:35 +0330

    add requirements.txt

---
+++ build.sh
@@ -0,0 +1,12 @@
+
+# exit on error
+set -o errexit
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+#python manage.py makemigrations
+python manage.py migrate
+
+python manage.py create_default_superuser
